fd0aa42c42fd225fb556f79b6663ec92d89c2b7c
Callum <callumbonnyman@me.com>
2021-12-10 17:59:05 +0200

    Adjusted hero text sizes for better responsive behavior.

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -11,15 +11,21 @@ const Hero: FC<HeroProps> = ({ title, subtitle }) => (
     <div className="max-w-4xl mx-auto text-center">
       <div className="block mx-auto mb-6">
         <Image
-          className="rounded-full shadow-md"
+          className="bg-white rounded-full shadow-md"
           src="/callum-bonnyman-square.jpg"
           alt="Callum Bonnyman"
           width={192}
           height={192}
         />
       </div>
-      <h1 className="text-3xl font-bold">{title}</h1>
-      {subtitle && <h2 className="mt-8 text-4xl leading-snug">{subtitle}</h2>}
+      <h1 className="text:lg md:text-xl  g:text-2xl xl:text-3xl font-bold">
+        {title}
+      </h1>
+      {subtitle && (
+        <h2 className="mt-8 text-xl md:text-2xl lg:text-3xl xl:text-4xl leading-snug">
+          {subtitle}
+        </h2>
+      )}
     </div>
   </section>
 );
